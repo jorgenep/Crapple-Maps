e8f5a6cb65fa52466c81f68aff53b483b0ccb5bc
alfredndongi <ndongian@udmercy.edu>
2025-04-21 23:22:49 -0400

    just some idea of how i am trying to structure out favorites

---
+++ app/src/main/java/com/example/crapple_maps/LegendaryLowlightsActivity.java
@@ -0,0 +1,31 @@
+package com.example.crapple_maps;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LegendaryLowlightsActivity extends AppCompatActivity {
+    ListView favoritesList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_legendary_lowlights);
+
+        favoritesList = findViewById(R.id.favoritesList); // Assuming your ListView has this ID
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // TODO: Load JSON, parse it, and set up adapter here once the rest is ready
+    }
+}
